3da56c9a778052ceed71f9680996e5a29c45d062
Jiahua Wang <jiahuawang7@gmail.com>
2016-07-22 14:12:34 -0700

    Don't log FPS by default

--- radial/src/main/java/net/faintedge/rube/screens/GameScreen.java
+++ radial/src/main/java/net/faintedge/rube/screens/GameScreen.java
@@ -89,7 +89,7 @@ public class GameScreen extends ScreenAdapter {
     context.getPhysicsSystem().step();
     context.getPhysicsSystem().process();
 
-    fpsLogger.log();
+//    fpsLogger.log();
   }
 
   @Override
